e296b360f483e5983dcd44dcaa31998cb6b68183
catzilla <catzillaorz@outlook.com>
2023-01-27 21:09:13 +0800

    feat: 0700.Search_in_a_Binary_Search_Tree.ts

---
+++ 0000-0099/0700.Search_in_a_Binary_Search_Tree.ts
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+class TreeNode {
+    val: number
+    left: TreeNode | null
+    right: TreeNode | null
+    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+        this.val = (val === undefined ? 0 : val)
+        this.left = (left === undefined ? null : left)
+        this.right = (right === undefined ? null : right)
+    }
+}
+
+// Recursion
+function searchBST(root: TreeNode | null, val: number): TreeNode | null {
+    // base  case
+    if (root == null) return null
+    if (val === root.val) return root
+    // recesion relation
+    if (val > root.val) {
+        return searchBST(root.right, val)
+    }
+    else {
+        return searchBST(root.left, val)
+    }
+
+};
